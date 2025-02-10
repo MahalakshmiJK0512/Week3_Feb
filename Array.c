c4da5244c42f5af45597229a1df89b0dda07242d
MahalakshmiJK0512 <163166565+MahalakshmiJK0512@users.noreply.github.com>
2025-02-10 16:00:54 +0530

    Array

--- Array.c
+++ Array.c
@@ -17,3 +17,25 @@ int main() {
     
     return 0;
 }
+//Sum of arrays//
+
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int i,n,sum =0;
+    printf("Enter the number of array elements:");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        printf("Enter the value:");
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<=n;i++){
+        sum += arr[i];
+        
+    }
+    printf("The sum of array value is %d\n",sum);
+    
+    return 0;
+}
